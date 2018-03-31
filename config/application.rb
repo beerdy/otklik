0b415007f3dcd461fa92af4f75ca6157dd99d233
mr.beerdy <mr.beerdy@gmail.com>
2018-04-01 00:44:52 +0300

    assets not working problem

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,8 @@ module OtklikPw
     
     # Don't generate system test files.
     config.generators.system_tests = nil
-
+    
+    config.assets.paths << Rails.application.root.join("app", "assets")
     config.assets.paths << Rails.root.join('lib', 'assets')
   end
 end
