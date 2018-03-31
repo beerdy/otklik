fae4e69709021e7533ce2564fbd8b1f2859509d5
mr.beerdy <mr.beerdy@gmail.com>
2018-04-01 02:12:08 +0300

    coder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  # Localize  
   before_action :set_locale
-
   attr_accessor :ln
- 
+  # END Localize
+  
   def initialize
     super
     read_content
@@ -13,8 +14,9 @@ class ApplicationController < ActionController::Base
   private
   def read_content
     Content.all.each do |c|
-      @headr = c if c.url == "headr"
+      @headr  = c if c.url == "headr"
       @localr = c if c.url == "localr"
+      @coder  = c if c.url == "coder"
     end
   end
 
