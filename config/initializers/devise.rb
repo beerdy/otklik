6558bd3063458d97c1faf6ab32582e99fd828010
mr.beerdy <mr.beerdy@gmail.com>
2018-03-31 22:11:26 +0300

    check devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -281,8 +281,4 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.authenticate_with do
-    warden.authenticate! scope: :user
-  end
-  config.current_user_method(&:current_user)
 end
