92ce8e9c3ade68ee81346a8789b930cc0bcf0cc2
mr.beerdy <mr.beerdy@gmail.com>
2019-09-18 20:17:53 +0300

    add proxy telegramm functional

---
+++ app/controllers/telegrams_controller.rb
@@ -0,0 +1,40 @@
+require 'net/http'
+
+class TelegramsController < ApplicationController
+  before_action :parse_telegrama, only: [:web_hook_proxy]
+  
+  def web_hook_proxy
+    # Net::HTTP.post_form( URI.parse('http://192.168.20.187:3000/api_chats/telegrams/web_hook'), @telegrama )
+    Net::HTTP.post_form( URI.parse('http://support.citykrepost.ru/api_chats/telegrams/web_hook'), @telegrama )
+
+    render json: { status: true }, status: :ok
+  end
+  
+  def send_message
+    Net::HTTP.post_form( URI.parse('https://api.telegram.org/bot925758676:AAFmWDq4w8UXpnSmgHKj1sg8jd0RT_oslo0/sendMessage'), {
+      'chat_id' => params[:chat_id],
+      'text'    => params[:text]
+    })
+
+    render json: { status: true }, status: :ok
+  end
+
+  private
+    def parse_telegrama
+      begin
+        text_arr   = params[:message][:reply_to_message][:text].split ':'
+        @telegrama = {
+          :chat_id    => params[:message][:chat][:id],
+          :text       => params[:message][:text],
+          :text_reply => text_arr[1],
+          :support_id => text_arr[0].to_i,
+          :error      => false
+        }
+      rescue Exception => e
+        @telegrama = {
+          error: true,
+          info:  "Error parse reply - #{e}"
+        }
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   resources :messages
   resources :contents
+  
   get 'welcome/index'
   root 'welcome#index'
+  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '/api_chats' do
+    resources :telegrams do
+      collection do
+        post :web_hook_proxy
+        post :send_message
+      end
+    end
+  end
+  
 end
