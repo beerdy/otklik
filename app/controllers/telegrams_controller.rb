d835ba1c709c58034417ae8a8d1a02b60038371b
castle.vrn <castle.vrn@gmail.com>
2020-06-25 17:27:43 +0300

    some fix

--- app/controllers/telegrams_controller.rb
+++ app/controllers/telegrams_controller.rb
@@ -1,15 +1,16 @@
 require 'net/http'
 
 class TelegramsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :parse_telegrama, only: [:web_hook_proxy]
   
   def web_hook_proxy
     # Net::HTTP.post_form( URI.parse('http://192.168.20.187:3000/api_chats/telegrams/web_hook'), @telegrama )
-    Net::HTTP.post_form( URI.parse('http://support.citykrepost.ru/api_chats/telegrams/web_hook'), @telegrama )
+    Net::HTTP.post_form( URI.parse('https://support.citykrepost.ru/api_chats/telegrams/web_hook'), @telegrama )
 
     render json: { status: true }, status: :ok
   end
-  
+
   def send_message
     Net::HTTP.post_form( URI.parse('https://api.telegram.org/bot925758676:AAFmWDq4w8UXpnSmgHKj1sg8jd0RT_oslo0/sendMessage'), {
       'chat_id' => params[:chat_id],
